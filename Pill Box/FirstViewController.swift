5e507aabd04281c9ef4f6bf940608e0e9d4bf8b8
Henry Jordan <hjordan35@users.noreply.github.com>
2015-04-12 15:08:57 -0500

    Initial Commit

---
+++ Pill Box/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pill Box
+//
+//  Created by Henry Jordan III on 4/12/15.
+//  Copyright (c) 2015 Henry Jordan III. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pill Box/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pill Box
+//
+//  Created by Henry Jordan III on 4/12/15.
+//  Copyright (c) 2015 Henry Jordan III. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
